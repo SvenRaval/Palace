33ca7308f645fa665918797d01130016980654ba
SvenRaval <99671825+SvenRaval@users.noreply.github.com>
2023-12-22 15:32:57 +0100

    Feat : Step1 complete

--- Park/ParkingLot.cs
+++ Park/ParkingLot.cs
@@ -2,6 +2,9 @@
 {
     public class ParkingLot
     {
-
+        public void Park(Vehicle vehicle)
+        {
+            Console.WriteLine($"Step 1.5 : [{GetType().Name}] put vehicle into parkedVehicle");
+        }
     }
 }
